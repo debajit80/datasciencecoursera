79727d6f62dc3c43dd02701dee6536cf409028de
debajit80 <debojeet@gmail.com>
2015-06-20 14:58:14 -0500

    adding solution of R programming assignment Part1

---
+++ pollutantmean.R
@@ -0,0 +1,17 @@
+pollutantmean <- function(directory, pollutant, ID=1:332){
+  
+  x <<- paste(getwd(),directory,sep="/",collpase=NULL)  ##points to the directory where data is stored; assumes working directory already points to the directory where the specdata folder is located
+    
+  y <<- paste(x,list.files(x),sep="",collapse=NULL) ##reads all the data files in the directory
+    
+  z <<- do.call(rbind,lapply(y,read.csv)) ##appends all datasets 
+    
+  w <<- data.frame(z)  ##converts dataset to a dataframe
+    
+  v <<- w[w$ID %in% c(ID),] ##subsets data based on ID input 
+    
+  mean <- mean(v[[pollutant]], na.rm=TRUE) ##calculates mean excluding misisng values on the subset data 
+    
+  mean ##prints mean 
+  
+  }        
