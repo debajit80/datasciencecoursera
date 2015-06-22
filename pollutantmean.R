25dffedaceaf4b9307383ab0ac4bfab8d6cfbdba
debajit80 <debojeet@gmail.com>
2015-06-21 21:45:44 -0500

    adding pollutant mean

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,8 +1,8 @@
-pollutantmean <- function(directory, pollutant, ID=1:332){
+pollutantmean <- function(directory, pollutant, ID){
   
-  x <- paste(getwd(),directory,sep="/",collpase=NULL)  ##points to the directory where data is stored; assumes working directory already points to the directory where the specdata folder is located
+  ## x <- paste(getwd(),directory,sep="/",collpase=NULL)  ##points to the directory where data is stored; assumes working directory already points to the directory where the specdata folder is located
     
-  y <- paste(x,list.files(x),sep="",collapse=NULL) ##reads all the data files in the directory
+  ## y <- paste(x,list.files(x),sep="",collapse=NULL) ##reads all the data files in the directory
     
   z <- do.call(rbind,lapply(y,read.csv)) ##appends all datasets 
     
